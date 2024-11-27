022bb0e62ac5481e07ee2dc7c6c4a0c09b436f1b
frarlo8 <farmentalo8@gmail.com>
2024-11-27 16:32:39 +0100

    feat: Add conditional logic for showing posts.
    Also added more test posts.

--- src/App.js
+++ src/App.js
@@ -2,23 +2,49 @@
 
 export default function App() {
 
-  const testPost = {
-    id: 1,
-    text: 'Hello world! Currently testing React.',
-    timestamp: 'a minute ago',
-    author: {
-      username: 'John Doe'
-    }
-  }
+  const testPosts = [
+      {
+          id: 1,
+          text: 'Hello world! Currently testing React.',
+          timestamp: 'a minute ago',
+          author: {
+              username: 'John Doe'
+          },
+      },
+      {
+          id: 2,
+          text: 'This is a second post',
+          timestamp: 'a year ago',
+          author: {
+              username: 'Jane Doe'
+          },
+      },
+      {
+          id: 3,
+          text: 'Hello from the beginning of time.',
+          timestamp: 'an epoch ago',
+          author: {
+              username: 'Linus Torvalds'
+          },
+      },
+  ]
 
   return (
       <>
         <h1>Microblog</h1>
-        <p>
-          <b>{testPost.author.username}</b> &mdash; {testPost.timestamp}
-          <br />
-          {testPost.text}
-        </p>
+          {testPosts.length === 0 ?
+              <p>There are no blog posts.</p>
+              :
+          testPosts.map(testPost => {
+              return (
+                  <p key={testPost.id}>
+                      <b>{testPost.author.username}</b> &mdash; {testPost.timestamp}
+                      <br/>
+                      {testPost.text}
+                  </p>
+              );
+          })
+          }
       </>
   );
 }
