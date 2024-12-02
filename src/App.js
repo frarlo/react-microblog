e4f2e954b422b16633655e3a2ac2da8892c6a512
frarlo8 <farmentalo8@gmail.com>
2024-12-02 10:15:16 +0100

    feat: Add Posts component.

--- src/App.js
+++ src/App.js
@@ -2,35 +2,9 @@ import Container from 'react-bootstrap/Container';
 import Stack from 'react-bootstrap/Stack';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
+import Posts from './components/Posts';
 
 export default function App() {
-
-  const testPosts = [
-      {
-          id: 1,
-          text: 'Hello world! Currently testing React.',
-          timestamp: 'a minute ago',
-          author: {
-              username: 'John Doe'
-          },
-      },
-      {
-          id: 2,
-          text: 'This is a second post',
-          timestamp: 'a year ago',
-          author: {
-              username: 'Jane Doe'
-          },
-      },
-      {
-          id: 3,
-          text: 'Hello from the beginning of time.',
-          timestamp: 'an epoch ago',
-          author: {
-              username: 'Linus Torvalds'
-          },
-      },
-  ]
 
   return (
       <Container fluid className="App">
@@ -39,19 +13,7 @@ export default function App() {
               <Stack direction="horizontal">
                   <Sidebar />
                   <Container>
-                      {testPosts.length === 0 ?
-                          <p>There are no blog posts.</p>
-                          :
-                          testPosts.map(testPost => {
-                              return (
-                                  <p key={testPost.id}>
-                                      <b>{testPost.author.username}</b> &mdash; {testPost.timestamp}
-                                      <br/>
-                                      {testPost.text}
-                                  </p>
-                              );
-                          })
-                      }
+                      <Posts />
                   </Container>
               </Stack>
           </Container>
---
+++ src/components/Posts.js
@@ -0,0 +1,49 @@
+
+
+export default function Posts() {
+
+    const testPosts = [
+        {
+            id: 1,
+            text: 'Hello world! Currently testing React.',
+            timestamp: 'a minute ago',
+            author: {
+                username: 'John Doe'
+            },
+        },
+        {
+            id: 2,
+            text: 'This is a second post',
+            timestamp: 'a year ago',
+            author: {
+                username: 'Jane Doe'
+            },
+        },
+        {
+            id: 3,
+            text: 'Hello from the beginning of time.',
+            timestamp: 'an epoch ago',
+            author: {
+                username: 'Linus Torvalds'
+            },
+        },
+    ]
+
+    return (
+        <>
+            {testPosts.length === 0 ?
+                <p>There are no blog posts.</p>
+                :
+                testPosts.map(testPost => {
+                    return (
+                        <p key={testPost.id}>
+                            <b>{testPost.author.username}</b> &mdash; {testPost.timestamp}
+                            <br/>
+                            {testPost.text}
+                        </p>
+                    );
+                })
+            }
+        </>
+    );
+}
