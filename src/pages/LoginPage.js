c3b9d7ba9a20dd6ca680958061b16f73eed23705
frarlo8 <farmentalo8@gmail.com>
2024-12-04 18:43:56 +0100

    feat: Add Page components.

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,11 @@
+import Body from '../components/Body';
+
+
+export default function LoginPage() {
+    return (
+      <Body>
+          <h1>Login form here</h1>
+          <p><i>TODO</i></p>
+      </Body>
+    );
+}
